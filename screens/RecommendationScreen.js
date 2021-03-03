5c41d67a941a9ae3374f494d3d9d2958d58a334c
mKol13134 <61067214+mkolowrat@users.noreply.github.com>
2021-03-03 14:28:52 -0800

    guest

--- screens/RecommendationScreen.js
+++ screens/RecommendationScreen.js
@@ -15,7 +15,7 @@ import BottomCards from "../components/BottomCards";
 import { firebase } from "../firebase";
 
 const RecommendationScreen = ({ route, navigation }) => {
-  const { name, uid } = route.params;
+  const { name, uid } = route.params;  //because we have a guest account 
   const [auth, setAuth] = useState();
   const [user, setUser] = useState();
   useEffect(() => {
